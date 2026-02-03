337f39781b18b3398b8298c638c465ee8409e4ab
Joshua <joshzam0830@gmail.com>
2026-02-03 15:58:13 -0600

    Adds basic system shortcuts and makes config more generic

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 set -e
 
 echo "Installing packages"
-sudo apt install -y vim i3 i3blocks git
+sudo apt install -y vim i3 i3blocks git pavucontrol blueman nautilus
 
 echo "Creating directories"
-mkdir -p ~/.config/i3 ~/.config/i3blocks
+mkdir -p ~/.config/i3 ~/.config/i3blocks    #Directory for i3
+mkdir -p ~/.local/bin                       #Directory for shortcuts 
 
 echo "Linking config files"
-ln -sf ~/.dotfiles/config/i3/config ~/.config/i3/config
-ln -sf ~/.dotfiles/config/i3blocks/config ~/.config/i3blocks/config
-ln -sf ~/.dotfiles/config/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/config/i3/config ~/.config/i3/config
+ln -sf ~/dotfiles/config/i3blocks/config ~/.config/i3blocks/config
+ln -sf ~/dotfiles/config/vim/vimrc ~/.vimrc
+
+echo "Adding basic shortcuts"
+ln -sf ~/dotfiles/shortcuts/* ~/.local/bin/ 
 
 echo "Done"
